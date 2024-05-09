f6b3520e44d47048845c1cf070c0f266739c2f2c
Araib <araib.saleem@gmail.com>
2024-05-09 16:59:29 -0400

    Creating ML agents asmdef

---
+++ Assets/Project/ML/MLAgentQualities.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+namespace BluMarble.ML
+{
+    [CreateAssetMenu(fileName = "BluMarble_MLAgentQualities", menuName = "BluMarbleScriptableObjects/BluMarble_MLAgentQualities")]
+    public class MLAgentQualities : ScriptableObject
+    {
+    }
+}
