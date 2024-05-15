feee2a65b6a8ed4a0a9e7811c9ad54392447102e
Araib <araib.saleem@gmail.com>
2024-05-15 14:25:09 -0400

    Adding different behaviors

---
+++ Assets/Project/Scripts/Society/InvestorBehavior.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+namespace BluMarble.Society
+{
+    public class InvestorBehavior : MonoBehaviour
+    {
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
